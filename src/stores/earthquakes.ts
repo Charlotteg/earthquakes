512553340afe827dc5dc1c571ed82c485cdbb6a0
Charlotteg <charlotte.graves@arup.com>
2024-04-07 14:00:00 +0100

    feat(state): :sparkles: load earthquake data into state via axios
    #5 load data into app

--- src/stores/earthquakes.ts
+++ src/stores/earthquakes.ts
@@ -1,14 +1,31 @@
 import { ref } from 'vue';
 import type { Ref } from 'vue';
 import { defineStore } from 'pinia';
+import axios from 'axios';
 import type { EarthquakeFeature } from '@/models/earthquake.model';
 
-export const useEarthquakeStore = defineStore('counter', () => {
+export const useEarthquakeStore = defineStore('earthquakes', () => {
   /* in this syntac refs become state properties
     computeds become getters
     functions become actions
   */
   const earthquakes: Ref<EarthquakeFeature[]> = ref([]);
+  const loading: Ref<boolean> = ref(false);
+  const loaded: Ref<boolean> = ref(false);
+  const getEarthquakes = async () => {
+    loading.value = true;
+    try {
+      const earthquakeRes = await axios.get(
+        `https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson`
+      );
+      earthquakes.value = earthquakeRes.data;
+      loading.value = false;
+      loaded.value = true;
+    } catch (error) {
+      console.log('Error loading earthquake data', error);
+      loading.value = false;
+    }
+  };
 
-  return { earthquakes };
+  return { earthquakes, loading, loaded, getEarthquakes };
 });
